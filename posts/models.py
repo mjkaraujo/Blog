4bcb4b3ecfe01dfc9758f8bb3052022e0b2b88cd
mjkaraujo <mjk_araujo@hotmail.com>
2022-08-20 18:50:46 +0000

    Correção de bug

--- posts/models.py
+++ posts/models.py
@@ -23,27 +23,3 @@ class Post(models.Model):
 
     def __str__(self):
         return self.titulo_post
-
-# Redimensionando a imagem salva no post
-    def save(self, *args, **kwargs):
-        super().save(*args, **kwargs)
-
-        self.resize_image(self.imagem_post, 800)
-
-    @staticmethod
-    def resize_image(img_name, new_width):
-        img_path = os.path.join(settings.MEDIA_ROOT, img_name)
-        img = Image.open(img_path)
-        width, height = img.size
-        new_height = round((new_width * height) / width)
-
-        if width <= new_width:
-            img.close()
-            return
-
-        new_img = img.resize((new_width, new_height), Image.ANTIALIAS)
-        new_img.save(
-            img_path,
-            optimize=True,
-            quality=60,
-        )
